2b94b747f0b325be4da1b1e11ce6f85f6b34a34d
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-26 11:32:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kullanıcının_girdiği_sayıların_ortalaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adet Sayısını Giriniz: ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            int sayi;
+            for (int i = 0; i < adet; i++)
+            {
+                Console.Write("Sayı Giriniz: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                toplam += sayi;
+            }
+            Console.WriteLine("Sayıların Ortalaması = " + (toplam / adet)); 
+            Console.ReadLine();
+            
+        }
+    }
+}
